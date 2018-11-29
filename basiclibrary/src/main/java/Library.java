866dac32c9d098ff686a66463fb74849fe8c1a66
RazorWire13 <baneblade13@gmail.com>
2018-11-29 13:44:32 -0800

    first draft

--- basiclibrary/src/main/java/Library.java
+++ basiclibrary/src/main/java/Library.java
@@ -1,4 +1,8 @@
 import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Collections;
 
 
 public class Library {
@@ -15,15 +19,53 @@ public class Library {
         return rollArray;
     }
 
-    public static boolean containsDuplicates(int[] rollArrayToCheckForDuplicates) {
-        for (int i = 0; i < rollArrayToCheckForDuplicates.length; i++) {
-            for (int j = i + 1; j < rollArrayToCheckForDuplicates.length; j++) {
-                if (rollArrayToCheckForDuplicates[i] == (rollArrayToCheckForDuplicates[j])) {
+    public static boolean containsDuplicates(int[] arrayToCheckDuplicates) {
+        for (int i = 0; i < arrayToCheckDuplicates.length - 1; i++) {
+            for (int j = i + 1; j < arrayToCheckDuplicates.length; j++) {
+                if (arrayToCheckDuplicates[i] == (arrayToCheckDuplicates[j])) {
                     return true;
                 }
             }
         }
         return false;
+    }
+
+    public static void minMaxInTwoDimArray() {
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        minMaxInTwoDimArray(weeklyMonthTemperatures);
+
+        Set<Integer> targetTempSet = new HashSet<Integer>();
+
+        for (int i = 0; i < weeklyMonthTemperatures.length; i++) {
+            for (int j = 0; j < weeklyMonthTemperatures[i].length; j++) {
+                if ( !targetTempSet.contains(weeklyMonthTemperatures[i][j]) ) {
+                    targetTempSet.add(weeklyMonthTemperatures[i][j]);
+                }
+            }
+            System.out.println(targetTempSet);
+
+            return targetTempSet;
+
+
+        }
+        int maxTemp = Collections.max(targetTempSet);
+        int minTemp = Collections.min(targetTempSet);
+        System.out.println("High: " + maxTemp);
+        System.out.println("Low: " + minTemp);
+
+        for (int n = minTemp; n < maxTemp; n++) {
+            if ( !targetTempSet.contains(n)) {
+                //  never += ("Never saw temp: " + n + "\n");
+                System.out.println("Never saw temperature: " + n);
+            }
+        }
+
+    }
     }
 
 }
