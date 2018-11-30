98c029a34fe356dd8872f80fea5ed1f5b59b3dde
RazorWire13 <baneblade13@gmail.com>
2018-11-30 13:34:51 -0800

    a lot of reading and not a lot of code

---
+++ bitmap-transformer/src/main/java/App.java
@@ -0,0 +1,14 @@
+
+public class Bitmap {
+
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+
+    }
+
+    public static String bitmapTransform() {
+        
+    }
+}
+
+
